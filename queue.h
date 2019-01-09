2094f3f3f1b8df2d6251839ac27c76d690cc10bf
r2newell <42938289+r2newell@users.noreply.github.com>
2019-01-08 23:42:51 -0500

    Update queue.h

--- queue.h
+++ queue.h
@@ -19,5 +19,4 @@ void enqueue(queue *, int);
 void dequeue(queue *, int *);
 int initializeQueue(queue*, int);//initialize queue to a specified size and returns zero if successful
 void freeQueue(queue*);// free memory used by queue
-void display(const queue*); 
 #endif
